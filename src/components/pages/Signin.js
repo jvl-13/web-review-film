be903fd6145c87930d341305fcaa5f28d5d994c2
Hue <19521558@gm.uit.edu.vn>
2022-06-14 15:02:40 +0700

    signin-signup

--- src/components/pages/Signin.js
+++ src/components/pages/Signin.js
@@ -1,66 +1,60 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-import Signin from '../Signin/Signin';
-import {ReactComponent as Logo} from '../../assets/instagram.svg'
-import './login.css';
-import { Route } from 'react-router-dom';
-class Login extends React.Component{
-    state={
-        email:'',
-        pwd:''
-    }
+import React from 'react'
+import logo from '../assets/images/movieLogo.png'
+import './Signin.css'
 
-    handleChange = (e) =>{
-        const {name,value} = e.target
-        this.setState({[name]:value})
-    }
-
-    handleSubmit = (e) =>{
-        e.preventDefault()
-        this.props.isLogin(true)
-    }
-    render(){
-        return(
-            <div className='login'>
-                {/* <div className='login-poster'>
-                    <img src='https://cdn.shopify.com/s/files/1/0581/5012/5749/products/fefab4_27d87781f56b4c50b6ef5c18e83c11e4_mv2_475x700.jpg?v=1625650528'/>
-                </div> */}
-            <div className='login-signin'>
-                <div className='login-logo'>
-                    <Logo/>
-                </div>
-                <div className='big-form'>
-                        <form onSubmit = {this.handleSubmit}>
-                        <input type='email' name='email' placeholder='email...' required onChange={this.handleChange}/>
-                        <input type='password' name='pwd' placeholder='password...' required onChange={this.handleChange}/>
-                        <button onSubmit={this.handleSubmit}>Login</button>
-                    </form>
-                    
-                </div>
-                <div>
-                    -------------------  or  -----------------<br/>
-                </div>
-                <div className='login-forgot-password'>
-                    <a href={'https://www.youtube.com/watch?v=7xuKwIa5x9Q'}>Forgot password?</a>
-                </div> 
+class Signin extends React.Component{
+  state={
+            email:'',
+            pwd:''
+        }
+    
+        handleChange = (e) =>{
+            const {name,value} = e.target
+            this.setState({[name]:value})
+        }
+    
+        handleSubmit = (e) =>{
+            e.preventDefault()
+            this.props.isLogin(true)
+        }
+  render(){ 
+    return(
+        <div className='login'>
+        <div className='login-signin'>
+            <div className='login-logo'>
+                <img className='logo' src={logo} alt='logo'></img>
             </div>
-            <Route>
-                <div className='login-signup'>
-                    <div>
-                        Don't have an account? <br/>
-                    </div>
-                    <div> 
-                        <Link to="/Pages/Signin">Signin</Link>
-                    </div>
-                </div>
-                <Route path="/Pages/Signin">
-                    <Signin />
-                </Route>
-            </Route>
-            
+            <div className='login-form'>
+                    <form onSubmit = {this.handleSubmit}>
+                    <input type='email' name='email' placeholder='email...' required onChange={this.handleChange}/>
+                    <input type='password' name='pwd' placeholder='password...' required onChange={this.handleChange}/>
+                    <button onSubmit={this.handleSubmit}>Login</button>
+                </form>
+                
             </div>
            
-        )
+            <div>
+                -----------------------  or  ----------------------<br/>
+            </div>
+            <div className='login-forgot-password'>
+                <a href={'https://www.youtube.com/watch?v=7xuKwIa5x9Q'}>Forgot password?</a>
+            </div> 
+        </div>
+        <div className='login-signup'>
+                <div>
+                    Don't have an account? <br/>
+                </div>
+                <div> 
+                    Sign up
+                    {/* <Link to="/pages/Signin">Signin</Link> */}
+                </div>
+            </div>
+        </div>   
+        )          
     }
 }
-export default Login;
+export default Signin
+
+
+
+
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,56 +1,52 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import {ReactComponent as Logo} from '../../assets/instagram.svg'
-import './Signup.css';
+import React from 'react'
+import logo from '../assets/images/movieLogo.png'
+import './Signup.css'
+
 class Signup extends React.Component{
-    state={
-        email:'',
-        pwd:'',
-        cfpwd:''
-    }
-
-    handleChange = (e) =>{
-        const {name,value} = e.target
-        this.setState({[name]:value})
-    }
-
-    handleSubmit = (e) =>{
-        e.preventDefault()
-        this.props.isLogin(true)
-    }
-    render(){
-        return(
-            <div className='signup'>
-                {/* <div className='Signup-poster'>
-                    <img src='https://cdn.shopify.com/s/files/1/0581/5012/5749/products/fefab4_27d87781f56b4c50b6ef5c18e83c11e4_mv2_475x700.jpg?v=1625650528'/>
-                </div> */}
-            <div className='signup-signup'>
-                <div className='signup-logo'>
-                    <Logo/>
-                </div>
-                <div className='big-form'>
-                    <form onSubmit = {this.handleSubmit}>
-                        <input type='email' name='email' placeholder='Email' required onChange={this.handleChange}/>
-                        <input type='password' name='pwd' placeholder='Password' required onChange={this.handleChange}/>
-                        <input type='password' name='cfpwd' placeholder='Confirm password' required onChange={this.handleChange}/>
-                        <button onSubmit={this.handleSubmit}>Sign up</button>
-                    </form>
-                    
-                </div>
+  state={
+            email:'',
+            pwd:'',
+            cfpwd:''
+        }
+    
+        handleChange = (e) =>{
+            const {name,value} = e.target
+            this.setState({[name]:value})
+        }
+    
+        handleSubmit = (e) =>{
+            e.preventDefault()
+            this.props.isLogin(true)
+        }
+  render(){ 
+    return(
+        <div className='signup'>
+        <div className='signup-signin'>
+            <div className='signup-logo'>
+                <img className='logo' src={logo} alt='logo'></img>
             </div>
-            <div className='signup-login'>
+            <div className='signup-form'>
+                    <form onSubmit = {this.handleSubmit}>
+                    <input type='email' name='email' placeholder='Email...' required onChange={this.handleChange}/>
+                    <input type='password' name='pwd' placeholder='Password...' required onChange={this.handleChange}/>
+                    <input type='password' name='cfpwd' placeholder='Confirm password...' required onChange={this.handleChange}/>
+                    <button onSubmit={this.handleSubmit}>Sign up</button>
+                </form>
+                
+            </div>
+            
+        </div>
+        <div className='signup-signup'>
                 <div>
                     Have an account? <br/>
-                    </div>
+                </div>
                 <div> 
-                    
-                    <Link to="/Signin">Login</Link>
-                    
+                    Sign in
+                    {/* <Link to="/pages/Signin">Signin</Link> */}
                 </div>
             </div>
-            </div>
-           
-        )
+        </div>   
+        )          
     }
 }
-export default Signup;
+export default Signup
